543401941c8657689fbccda69a645d88235bd64f
Connor <connor_ford1@baylor.edu>
2023-01-25 13:13:22 -0600

    added hello world print statement

--- MyGraph.py
+++ MyGraph.py
@@ -9,3 +9,11 @@ plt.show()
 print("Hello there again!")
 
 print("Hi!")
+
+print("Hello World")
+
+
+# Step 1: Clone a repo
+# Step 2: Create a Virtual Enviroment: "py -3 -m venv .venv" last bit is name of the VE, nothing special ab period
+# Step 3: activate your VE: get to activate script, .venv/scripts/activate
+# Step 4: Installing 3rd party libraries "pip3 install _________"
